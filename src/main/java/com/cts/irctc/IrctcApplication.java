189dc2810919d36a36271b941aec16ef56298bc9
HarshRateriaFSE <harshrateria7975@gmail.com>
2022-04-07 22:26:54 +0530

    comment added

--- src/main/java/com/cts/irctc/IrctcApplication.java
+++ src/main/java/com/cts/irctc/IrctcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IrctcApplication {
-
+//harsh rateria
 	public static void main(String[] args) {
 		SpringApplication.run(IrctcApplication.class, args);
 		
